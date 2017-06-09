8160c5f1e7b4ae9eaaf38f60ac8cc42a06db4309
Rebecca <mostsweethoney@qq.com>
2017-06-09 18:48:00 +0800

    upload build

--- src/components-todoapp/filter-types.js
+++
@@ -1,3 +0,0 @@
-export const SHOW_ALL = 'all';
-export const SHOW_ACTIVE = 'active';
-export const SHOW_COMPLETED ='completed';
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React, {Component} from 'react';
-import ReactDom, {render} from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import TodoApp from './components-todoapp/TodoApp';
-render(<TodoApp/>,document.querySelector('#root'));
